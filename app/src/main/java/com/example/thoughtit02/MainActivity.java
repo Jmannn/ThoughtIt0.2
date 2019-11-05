feb2727acd33c5a60707f8cb4a4c8f3f2bd0fca3
Jmannn <manjo773@student.otago.ac.nz>
2019-11-05 21:31:58 +0000

    Added basic save methods. Removed testing stuff. Basic interface works.

--- app/src/main/java/com/example/thoughtit02/MainActivity.java
+++ app/src/main/java/com/example/thoughtit02/MainActivity.java
@@ -7,16 +7,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private static final String TAG = "MainActivity";
 
     private List<String> thoughts = new ArrayList<>();
     private List<Date> dates = new ArrayList<>();
+    private RecyclerViewAdaptor adaptor;
+    private EditText textBox;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +29,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreatet start");
         initThoughts();
+        this.textBox = findViewById(R.id.text_enter);
     }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "- ON PAUSE -");
+    }
+    //use this for model loading of prev thoughts empty on first open
     private void initThoughts(){
         Log.d(TAG, "Creating thoughts");
         String str = "";
-        for (int i = 0; i < 10; i++) {
-            str = "This is the news " + i + " " + i * i;
-            str += " 123478912347891234789123478912347891234789123478912347891234789";
-            str += "AS you can see i know a lot about cars trucks";
-            dates.add(new Date());
-            thoughts.add(str);
-        }
+
         initRecyclerView();
     }
     private void initRecyclerView(){
         Log.d(TAG,  "initRecyclerView: init recyclerview.");
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        RecyclerViewAdaptor adaptor = new RecyclerViewAdaptor(this, thoughts, dates);
+        this.recyclerView = findViewById(R.id.recycler_view);
+        this.adaptor = new RecyclerViewAdaptor(this, thoughts, dates);
         recyclerView.setAdapter(adaptor);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.hasFixedSize();
     }
+    public void saveThought(View view){
+        Log.d(TAG,"saveThought");
+        this.thoughts.add(this.textBox.getText().toString());
+        this.textBox.setText("");
+        this.dates.add(new Date());
+        this.adaptor.notifyItemInserted(this.thoughts.size()-1);
+        this.recyclerView.scrollToPosition(this.thoughts.size()-1);
+
+    }
 }
+//TODO: Only display the thoughts of each day.
+//TODO: Have a toolbar drop down that can choose the date to display
+// but if the dates dont match you cant add, maybe hide components
+// multiple drop downs which have columns for year, month and day
